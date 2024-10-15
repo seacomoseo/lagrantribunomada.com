5795936ca3f40dec56dd72602c7e991862b7db2b
lorensansol <lorensansol@gmail.com>
2024-10-15 20:44:37 +0200

    Update: 2024-10-15 20:44:37

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@ import { scrollShot } from './scroll-shot'
 import { loadScript } from './load-script'
 import { loadStyle } from './load-style'
 
-// MAPA
+// MAPA GENERAL
 const mapa = document.getElementById('map')
 if (mapa) {
   function mapStart() {
@@ -33,7 +33,7 @@ if (mapa) {
     const oms = new OverlappingMarkerSpiderfier(map)
 
     // fetch data
-    fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vT_r-PYs-o_6cFTrBvwk1e-Q6Jq9ZH_HraPeyRUobkx392OUesn9WJSVwyX72udT5BDn__rCDGMq5W6/pub?gid=805672466&single=true&output=tsv')
+    fetch('/index.json')
       .then(response => response.json())
       .then(data => {
         // icons
@@ -53,23 +53,20 @@ if (mapa) {
 
         function content (type, color) {
           const group = []
-          data[type] && data[type].forEach(item => {
-            if (item.name) {
+          data.project.forEach(item => {
+            if (item.title && item.category[0].title === type) {
+              console.log(item.title)
               let content = ''
-              content += '<h3>' + item.name + '</h3>'
+              if (item.image) content += `<img src="${item.image}" width=512 height=512>`
+              content += `<h3 class="h5"><a href="${item.link}">${item.title}</a></h3>`
               if (item.summary) content += `<p>${item.summary}</p>`
-              if (item.web) content += `<p class="map-data"><svg><use xlink:href="/draws.svg#arrow-pointer"></use></svg> <a href="${item.web}" target="_blank">${item.web}</a></p>`
-              if (item.phone) content += `<p class="map-data"><svg><use xlink:href="/draws.svg#phone"></use></svg> <a href="tel:${item.phone.replace(/\D/g, '')}" target="_blank">${item.phone}</a></p>`
-              if (item.whatsapp) content += `<p class="map-data"><svg><use xlink:href="/draws.svg#whatsapp"></use></svg> <a href="https://wa.me/${item.whatsapp.replace(/\D/g, '')}" target="_blank">${item.whatsapp}</a></p>`
-              if (item.location) content += `<p class="map-data"><svg><use xlink:href="/draws.svg#location-dot"></use></svg> <a href="https://maps.google.com/maps/search/${item.name}, ${item.location}" target="_blank">${item.location}</a></p>`
-              if (item.description) content += `<h3>Descripción</h3><p>${item.description}</p>`
-              if (item.advantages) content += `<h3>Ventajas para socios</h3><p>${item.advantages}</p>`
-              if (item.visitable) content += `<h3 class="compare">Acepta visitas <svg><use xlink:href="/draws.svg#check"></use></svg></h3>`
-              if (item.observations) content += `<h3>Observaciones</h3><p>${item.observations}</p>`
-              content = content
-                .replace(/(?<=(<br>|<p>))- (.*?)(?=(<br>|<\/p>))/g, '<span class="map-list"><svg><use xlink:href="/draws.svg#hyphen"></use></svg> $2</span>')
-                .replace(/\n/g, '<br>')
-              const marker = L.marker(item.coordinates, { icon: icon(color) }).bindPopup(content/*, { maxHeight: 400 }*/)
+              if (item.visitable) content += '<h3 class="compare">Acepta visitas <svg><use xlink:href="/draws.svg#check"></use></svg></h3>'
+              // content += `<p><svg><use href="/draws.svg#bookmark"></use></svg> Categoría: <a class="alt" href="${item.category[0].link}">${item.category[0].title}</a></p>`
+              content += `<p><a class="button alt" href="${item.link}"><svg><use href="/draws.svg#circle-info"></use></svg> MÁS</a></p>`
+              // content = content
+              //   .replace(/(?<=(\n|<p>))- (.*?)(?=(\n|<\/p>))/g, '<span class="map-list"><svg><use xlink:href="/draws.svg#hyphen"></use></svg> $2</span>')
+              //   .replace(/\n/g, '<br>')
+              const marker = L.marker([item.address.x, item.address.y], { icon: icon(color) }).bindPopup(content/*, { maxHeight: 400 }*/)
               oms.addMarker(marker)
               group.push(marker)
             }
@@ -79,11 +76,11 @@ if (mapa) {
 
         // layer control
         const overlayMaps = {
-          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png" height="16" width="8" alt="Marker"> Comunidades': content('communities', 'gold'),
-          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png" height="16" width="8" alt="Marker"> Espacios': content('spaces', 'violet'),
-          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png" height="16" width="8" alt="Marker"> Solo Servicios': content('services', 'red'),
-          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png" height="16" width="8" alt="Marker"> Otros': content('others', 'blue'),
-          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png" height="16" width="8" alt="Marker"> Puntos de Interés': content('interest', 'green')
+          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png" height="16" width="8" alt="Marker"> Comunidades': content('Comunidades', 'gold'),
+          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png" height="16" width="8" alt="Marker"> Espacios': content('Espacios', 'violet'),
+          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png" height="16" width="8" alt="Marker"> Solo Servicios': content('Servicios', 'red'),
+          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png" height="16" width="8" alt="Marker"> Otros': content('Otros', 'blue'),
+          // '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png" height="16" width="8" alt="Marker"> Puntos de Interés': content('Interés', 'green')
         }
         const layerControl = L.control.layers(null, overlayMaps).addTo(map)
 
@@ -108,6 +105,38 @@ if (mapa) {
         // https://github.com/jawj/OverlappingMarkerSpiderfier-Leaflet
         loadScript('https://cdnjs.cloudflare.com/ajax/libs/OverlappingMarkerSpiderfier-Leaflet/0.2.6/oms.min.js', mapStart)
       })
+    }
+  })
+}
+
+// MAPA INIVIDUAL
+const mapProject = document.getElementById('map-project')
+if (mapProject) {
+  function mapStart () {
+    const x = mapProject.dataset.x
+    const y = mapProject.dataset.y
+    const isMobile = window.innerWidth <= 768 // Definición de tamaño móvil (puedes ajustarlo)
+    const initialZoom = isMobile ? 4 : 5 // Ajuste de zoom según dispositivo
+    // eslint-disable-next-line
+    const map = L.map('map-project', {
+      setView: true,
+      trackResize: true
+    }).setView([x, y], initialZoom)
+
+    // eslint-disable-next-line
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map)
+
+    let marker = L.marker([x, y]).addTo(map)
+  }
+
+  scrollShot({
+    rootMargin: '0%',
+    query: '#map-project',
+    doStart: gallery => {
+      loadStyle('https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', null)
+      loadScript('https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', mapStart)
     }
   })
 }
