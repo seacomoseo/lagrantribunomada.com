a676e1d6ac2a50e32497a2fec2378aa503be349a
lorensansol <lorensansol@gmail.com>
2024-10-25 05:06:55 +0200

    Update: 2024-10-25 05:06:55

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,12 +5,15 @@ import { loadStyle } from './load-style'
 // MAPA GENERAL
 const mapa = document.getElementById('map')
 if (mapa) {
-  function mapStart() {
+  // Definir el objeto donutClusters fuera de la función mapStart para evitar problemas de referencia
+  let donutCluster
+
+  function mapStart () {
     // zoom
     const minWidth = 600
     const maxWidth = 1300
     const minZoom = 5
-    const maxZoom = 7
+    const maxZoom = 6.5
     const calculateZoom = (width) => {
       if (width <= minWidth) return minZoom
       if (width >= maxWidth) return maxZoom
@@ -19,40 +22,57 @@ if (mapa) {
     const initialZoom = calculateZoom(window.innerWidth)
 
     // base
+    // eslint-disable-next-line
     const map = L.map('map', {
       zoomSnap: 0.5, // Permite valores fraccionarios para el nivel de zoom
       zoomDelta: 0.5,
-      trackResize: true
+      trackResize: true,
+      scrollWheelZoom: false
     }).setView([40.007, -2.488], initialZoom)
 
+    // eslint-disable-next-line
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map)
 
-    // spiderfier
-    const oms = new OverlappingMarkerSpiderfier(map)
+    // Crear un solo cluster para todos los marcadores
+    donutCluster = L.DonutCluster({
+      chunkedLoading: true
+    }, {
+      key: 'category',
+      arcColorDict: {
+        Comunidades: 'cornflowerblue',
+        Espacios: 'green',
+        Servicios: 'crimson',
+        Otros: 'violet'
+      },
+      style: {
+        size: 40,
+        fill: 'var(--dark)',
+        opacity: 1,
+        weight: 7
+      },
+      textContent: 'sum'
+    }).addTo(map)
 
     // fetch data
     fetch('/index.json')
       .then(response => response.json())
       .then(data => {
         // icons
-        function icon (color) {
-          return new L.Icon({
-            iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
-            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-            iconSize: [25, 41],
-            iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowSize: [41, 41]
+        function icon (color, iconId) {
+          // eslint-disable-next-line
+          return L.divIcon({
+            className: 'leaflet-data-marker',
+            // eslint-disable-next-line
+            html: `<svg class="leaflet-data-marker__svg" viewBox="0 -5 149 188"><path fill="${color}" stroke="white" stroke-width="12" paint-order="stroke" stroke-miterlimit="10" d="M126 23l-6-6A69 69 0 0 0 74 1a69 69 0 0 0-51 22A70 70 0 0 0 1 74c0 21 7 38 22 52l43 47c6 6 11 6 16 0l48-51c12-13 18-29 18-48 0-20-8-37-22-51z"/><use fill="white" href="/draws.svg#${iconId}" x="36" y="16" transform="scale(.67)"></use></svg>`,
+            iconAnchor: [24, 50],
+            iconSize: [48, 48],
+            popupAnchor: [0, -46]
           })
         }
-        function iconLayer (color) {
-          return `<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png" height="16" width="8" alt="Marker">`
-        }
 
-        function content (type, color) {
-          const group = []
+        function addMarkersToCluster(type, color, iconId) {
           data.project.forEach(item => {
             if (item.title && item.category[0].title === type) {
               let content = ''
@@ -60,26 +80,47 @@ if (mapa) {
               content += `<h3 class="h5"><a href="${item.link}">${item.title}</a></h3>`
               if (item.summary) content += `<p>${item.summary}</p>`
               if (item.visitable) content += '<h3 class="compare">Acepta visitas <svg><use xlink:href="/draws.svg#check"></use></svg></h3>'
-              // content += `<p><svg><use href="/draws.svg#bookmark"></use></svg> Categoría: <a class="alt" href="${item.category[0].link}">${item.category[0].title}</a></p>`
               content += `<p><a class="button alt" href="${item.link}"><svg><use href="/draws.svg#circle-info"></use></svg> MÁS</a></p>`
               content = content.replace(/<li>(.*?)<\/li>/g, '<li class="li-svg"><svg><use xlink:href="/draws.svg#hyphen"></use></svg> <div>$1</div></li>')
-              const marker = L.marker([item.address.x, item.address.y], { icon: icon(color) }).bindPopup(content/*, { maxHeight: 400 }*/)
-              oms.addMarker(marker)
-              group.push(marker)
+              // eslint-disable-next-line
+              const marker = L.marker([item.address.x, item.address.y], { icon: icon(color, iconId), title: item.title }).bindPopup(content)
+              marker.feature = { properties: { category: type } } // Añadir categoría como propiedad del marcador
+              donutCluster.addLayer(marker)
             }
           })
-          return L.layerGroup(group).addTo(map)
         }
+
+        // Añadir marcadores al cluster para cada tipo
+        addMarkersToCluster('Comunidades', 'cornflowerblue', 'people-group')
+        addMarkersToCluster('Espacios', 'green', 'campground')
+        addMarkersToCluster('Servicios', 'crimson', 'bullseye-arrow')
+        addMarkersToCluster('Otros', 'violet', 'hands-holding-heart')
 
         // layer control
         const overlayMaps = {
-          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png" height="16" width="8" alt="Marker"> Comunidades': content('Comunidades', 'gold'),
-          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png" height="16" width="8" alt="Marker"> Espacios': content('Espacios', 'violet'),
-          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png" height="16" width="8" alt="Marker"> Solo Servicios': content('Servicios', 'red'),
-          '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png" height="16" width="8" alt="Marker"> Otros': content('Otros', 'blue'),
-          // '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png" height="16" width="8" alt="Marker"> Puntos de Interés': content('Interés', 'green')
+          '<svg class="leaflet-data-marker__svg" viewBox="0 -5 149 188"><path fill="cornflowerblue" stroke="white" stroke-width="12" paint-order="stroke" stroke-miterlimit="10" d="M126 23l-6-6A69 69 0 0 0 74 1a69 69 0 0 0-51 22A70 70 0 0 0 1 74c0 21 7 38 22 52l43 47c6 6 11 6 16 0l48-51c12-13 18-29 18-48 0-20-8-37-22-51z"/><use fill="white" href="/draws.svg#people-group" x="36" y="16" transform="scale(.67)"></use></svg> Comunidades': donutCluster,
+          '<svg class="leaflet-data-marker__svg" viewBox="0 -5 149 188"><path fill="green" stroke="white" stroke-width="12" paint-order="stroke" stroke-miterlimit="10" d="M126 23l-6-6A69 69 0 0 0 74 1a69 69 0 0 0-51 22A70 70 0 0 0 1 74c0 21 7 38 22 52l43 47c6 6 11 6 16 0l48-51c12-13 18-29 18-48 0-20-8-37-22-51z"/><use fill="white" href="/draws.svg#campground" x="36" y="16" transform="scale(.67)"></use></svg> Espacios': donutCluster,
+          '<svg class="leaflet-data-marker__svg" viewBox="0 -5 149 188"><path fill="crimson" stroke="white" stroke-width="12" paint-order="stroke" stroke-miterlimit="10" d="M126 23l-6-6A69 69 0 0 0 74 1a69 69 0 0 0-51 22A70 70 0 0 0 1 74c0 21 7 38 22 52l43 47c6 6 11 6 16 0l48-51c12-13 18-29 18-48 0-20-8-37-22-51z"/><use fill="white" href="/draws.svg#bullseye-arrow" x="36" y="16" transform="scale(.67)"></use></svg> Solo Servicios': donutCluster,
+          '<svg class="leaflet-data-marker__svg" viewBox="0 -5 149 188"><path fill="violet" stroke="white" stroke-width="12" paint-order="stroke" stroke-miterlimit="10" d="M126 23l-6-6A69 69 0 0 0 74 1a69 69 0 0 0-51 22A70 70 0 0 0 1 74c0 21 7 38 22 52l43 47c6 6 11 6 16 0l48-51c12-13 18-29 18-48 0-20-8-37-22-51z"/><use fill="white" href="/draws.svg#hands-holding-heart" x="36" y="16" transform="scale(.67)"></use></svg> Otros': donutCluster
         }
+
+        // eslint-disable-next-line
         const layerControl = L.control.layers(null, overlayMaps).addTo(map)
+
+        // Controlar la visibilidad de las capas
+        map.on('overlayadd', function (eventLayer) {
+          const layerName = eventLayer.name
+          if (donutCluster) {
+            map.addLayer(donutCluster)
+          }
+        })
+
+        map.on('overlayremove', function (eventLayer) {
+          const layerName = eventLayer.name
+          if (donutCluster) {
+            map.removeLayer(donutCluster)
+          }
+        })
 
         // popup over top elements when popupopen in small screen sizes
         const mapPane = document.querySelector('.leaflet-map-pane')
@@ -94,49 +135,19 @@ if (mapa) {
     rootMargin: '0%',
     query: '#map',
     doStart: gallery => {
-      // https://github.com/Leaflet/Leaflet.markercluster
-      // loadStyle('https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css', null)
-      // loadScript('https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js', null)
+      // https://github.com/kalisio/Leaflet.DonutCluster
       loadStyle('https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', null)
+      loadStyle('https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css', null)
+      loadStyle('https://cdn.jsdelivr.net/npm/@kalisio/leaflet.donutcluster@1.1.0/src/Leaflet.DonutCluster.min.css', null)
       loadScript('https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', () => {
-        // https://github.com/jawj/OverlappingMarkerSpiderfier-Leaflet
-        loadScript('https://cdnjs.cloudflare.com/ajax/libs/OverlappingMarkerSpiderfier-Leaflet/0.2.6/oms.min.js', mapStart)
+        loadScript('https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js', () => {
+          loadScript('https://cdn.jsdelivr.net/npm/@kalisio/leaflet.donutcluster@1.1.0/src/Leaflet.DonutCluster.min.js', mapStart)
+        })
       })
     }
   })
 }
 
-// MAPA INIVIDUAL
-const mapProject = document.getElementById('map-project')
-if (mapProject) {
-  function mapStart () {
-    const x = mapProject.dataset.x
-    const y = mapProject.dataset.y
-    const isMobile = window.innerWidth <= 768 // Definición de tamaño móvil (puedes ajustarlo)
-    const initialZoom = isMobile ? 4 : 5 // Ajuste de zoom según dispositivo
-    // eslint-disable-next-line
-    const map = L.map('map-project', {
-      setView: true,
-      trackResize: true
-    }).setView([x, y], initialZoom)
-
-    // eslint-disable-next-line
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map)
-
-    let marker = L.marker([x, y]).addTo(map)
-  }
-
-  scrollShot({
-    rootMargin: '0%',
-    query: '#map-project',
-    doStart: gallery => {
-      loadStyle('https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', null)
-      loadScript('https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', mapStart)
-    }
-  })
-}
 
 // ALTA
 const formAlta = document.getElementById('form-alta')
